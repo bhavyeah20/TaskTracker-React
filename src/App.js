f717a59ce7bc059a5616fc7723c7f28d062b12c6
Bhavya Chaudhary <bhavyabbps14@gmail.com>
2021-02-06 23:44:11 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
 
-function App() {
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+
+const App = () => {
+
+  const[showAddTask,setShowAddTask] = useState(false)
+
+  //global state
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () =>{
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
+  },[])
+
+  //fetch tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+    return data
+  }
+
+   //fetch task
+   const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+    return data
+  }
+
+  //add task
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', { 
+      method: 'POST',
+      headers: {
+        'Content-type' : 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json()
+
+    setTasks([...tasks,data])
+    
+    // const id = Math.floor(Math.random() * 10000) + 1;
+    // const newTask = {id, ...task};
+    // setTasks([...tasks, newTask]);
+  }
+
+  //delete task
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`,{ method: 'DELETE'})
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  //toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task,reminder: !task.reminder} : task))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask &&  <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Tasks To Show')}
     </div>
   );
 }
